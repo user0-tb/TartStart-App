80b35afdc7298177cc19f1a78f7ded5ae1015c9f
User0 <104380885+user0-tb@users.noreply.github.com>
2024-03-21 15:31:46 +0100

    Update Widget.java

--- app/src/main/java/com/example/webview/Widget.java
+++ app/src/main/java/com/example/webview/Widget.java
@@ -1,4 +1,10 @@
-package com.tiledesk.tiledeskwidgetintegrationexample;
+package com.example.webview;
+
+import static android.app.PendingIntent.getActivity;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
